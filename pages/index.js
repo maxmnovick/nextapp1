1f2267227967ab243544d21f41f294632483b6f4
M <maxmnovick@gmail.com>
2022-10-10 14:08:51 -0400

    test build settings

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,9 @@ import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
+  //call api
+
+
   return (
     <div className={styles.container}>
       <Head>
